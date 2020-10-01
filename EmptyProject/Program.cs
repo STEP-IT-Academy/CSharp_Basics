c56bfa85454a8d5de3eef1494c6828f4efff1f33
Константин <kotkat.lv@gmail.com>
2020-10-01 13:28:18 +0300

    commit

--- EmptyProject/Program.cs
+++ EmptyProject/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace EmptyProject
+﻿using System;
+
+namespace EmptyProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Empty C# project");
         }
     }
 }
